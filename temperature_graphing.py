81d94ab96d64eac7e3fc01e3b1952074c8242313
Jesse Bishop <jessebishop@yahoo.com>
2013-06-02 13:31:45 -0400

    still not working

--- temperature_graphing.py
+++ temperature_graphing.py
@@ -1,7 +1,9 @@
-#!/bin/env python
+#!/usr/bin/python
 
-import psycopg2
+import matplotlib, psycopg2
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib import dates
 
 db = psycopg2.connect(host='localhost', database='jessebishop',user='jessebishop')
 cursor = db.cursor()
@@ -15,10 +17,27 @@ for sensor in sensors:
 	#cursor.execute("""SELECT name FROM temperature_sensors WHERE sensor_id = '%s';""" % sensor)
 	#sensorname = cursor.fetchone()[0]
 	sensorname = 'testing'
-	#cursor.execute("""SELECT measurement_time, temperature * 9 / 5 + 32 AS temp FROM temperature_measurement WHERE sensor_id = '%s';""" % sensor)
-	cursor.execute("""SELECT read_time, temperature * 9 / 5 + 32 AS temp FROM temperature_test WHERE device_id = '%s';""" % sensor)
+	print sensor
+	cursor.execute("""SELECT read_time, temperature * 9 / 5 + 32 AS temp FROM temperature_test WHERE device_id = '%s' AND read_time = CURRENT_TIMESTAMP - (7 * interval '1 day') ORDER BY id;""" % sensor)
 	data = cursor.fetchall()
+	if not data:
+		continue
 	timestamp, temperature = zip(*data)
+	dts = dates.date2num(timestamp)
+	hfmt = dates.DateFormatter('%Y-%m-%d %H:%M:%S')
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(dts,temperature)
+	ax.xaxis.set_major_locator(dates.MinuteLocator())
+	ax.xaxis.set_major_formatter(hfmt)
+	#ax.set_ylim(bottom = min(temperature) - 1)
+	#plt.show()
+
+	#plt.plot(dts,temperature)
+	#plt.xlim(min(dates), max(dates))
+
+	plt.grid()
+	plt.savefig('/var/www/electricity/' + sensor + '.png')
 
 
 
